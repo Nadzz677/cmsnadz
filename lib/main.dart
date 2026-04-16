e8e7c5a323c5c67946cd756c3e8b04980bdec1ba
Nadzz67 <nadzhaa254@gmail.com>
2026-04-16 19:26:03 +0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,54 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Container(
-            height: 220,
-            width: 220,
-            margin: EdgeInsets.symmetric(
-                vertical: 50.0,
-            horizontal: 10.0),
-            color: Colors.white,
-            child: Text('Muhammad Nadzha',
-            style: TextStyle(
-                fontSize: 40
-            ),
-          ),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 200,
+                backgroundImage: AssetImage('images/bagas-recall.gif'),
+              ), Icon(Icons.face, size: 40,),
+              Container(
+                height: 80,
+                width: 700,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(20.0)
+                ),
+                margin: EdgeInsets.symmetric(
+                    vertical: 50.0,
+                    horizontal: 10.0),
+                child: Center(
+                  child: Text('Muhammad Nadzha bin Mohamad Nasrul',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                        fontSize: 20,
+
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 80,
+                width: 700,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20.0)
+                ),
+                margin: EdgeInsets.symmetric(
+                    vertical: 50.0,
+                    horizontal: 10.0),
+                child: Center(
+                  child: Text('Muhammad Nadzha',
+                    style: TextStyle(
+                      fontFamily: 'PoiretOne',
+                        fontSize: 40
+
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
           ),
         ),
       ),
